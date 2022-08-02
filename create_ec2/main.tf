a1918b0566b3d85ebd7eceb56e88bbab8bb43ceb
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-08-03 01:54:00 +0200

    add main route table to VPC

--- create_ec2/main.tf
+++ create_ec2/main.tf
@@ -27,17 +27,18 @@ resource "aws_subnet" "my_app_subnet-1" {
 resource "aws_internet_gateway" "my_vpc_internet_gateway" {
      vpc_id = aws_vpc.my_app_vpc.id
      tags = {
-     "Name" = "${var.env_prefix}-internet-gateway"
-   }
+     "Name" = "${var.env_prefix}-internet-gateway"}
 }
 
- resource "aws_route_table" "my_app_route_table" {
-   vpc_id = aws_vpc.my_app_vpc.id
+
+resource "aws_default_route_table" "main-route-table" {
+  default_route_table_id = aws_vpc.my_app_vpc.default_route_table_id
+
    route {
     cidr_block = "0.0.0.0/0"
     gateway_id =aws_internet_gateway.my_vpc_internet_gateway.id
    }
    tags = {
-     "Name" = "${var.env_prefix}-vpc-route-table"
+     "Name" = "${var.env_prefix}-main-route-table"
    }
- }
+}
